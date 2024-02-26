69a709c81c7231568081cbb983084b08939135af
yfchen <chenyifanxjtu.phy@gmail.com>
2024-02-26 19:45:03 +0800

    Implement Navagation Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
+import Navbar from "@/components/Navbar";
 import { NextPageContext } from "next";
-import { getSession, signOut } from "next-auth/react";
-
-import useCurrentUser from "@/hooks/useCurrentUser";
+import { getSession } from "next-auth/react";
 
 // fetch our session from client side
 // cannot use serverAuth
@@ -25,13 +24,10 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data : user } = useCurrentUser();
 
   return (
     <>
-      <h1 className="text-4xl text-green-500">Netflix Clone</h1>
-      <p className="text-white">Logged in as : {user?.email}</p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout!</button>
+      <Navbar />
     </>
   );
 }
